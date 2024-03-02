ce8c9f39736dee11ab28fa531d23307b5332369a
afaqalii <afaqali4244@gmail.com>
2024-03-02 19:16:44 +0500

    Done with some changes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,6 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
-
 export default async function Page() {
   return (
     <main>
